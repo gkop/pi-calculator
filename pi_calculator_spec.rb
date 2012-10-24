62c8d46006b6db24269432f979425909ff20365e
Gabe Kopley <gabe@coshx.com>
2012-10-23 18:12:08 -0700

    Skeleton template

---
+++ pi_calculator_spec.rb
@@ -0,0 +1,8 @@
+$:<< "."
+require 'pi_calculator'
+
+describe PiCalculator, "#some_method" do
+  it "returns something close to pi" do
+    pending "What do we call our method and how do we know it's correct?"
+  end
+end
