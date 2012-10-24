f13794d803fbaf35f4083ee0150212fc31f6dfac
Gabe Kopley <gabe@coshx.com>
2012-10-23 20:41:29 -0700

    Pi!

--- pi_calculator.rb
+++ pi_calculator.rb
@@ -1,2 +1,33 @@
 class PiCalculator
+
+  def calculate
+    # calculate pi in terms of the area of a unit square and the circle
+    # inscribed within
+    square_area = 1
+    # circle radius is 0.5
+    # circle diameter is 1.0
+
+    points_in_circle = 0
+    num_points = 10000000
+    origin = {:x => 0.5, :y => 0.5}
+
+    num_points.times do
+      # this is where we a draw a point
+
+      point = {:x => rand, :y => rand}
+
+      # what do we want to know about this point?
+      # we want to know if it's in the circle
+      # IE we want know if the distance of the point from the origin is
+      # less than the radius of the circle
+      distance = Math.sqrt((point[:x] - origin[:x])**2 + (point[:y] - origin[:y])**2)
+
+      if distance < 0.5
+        points_in_circle += 1
+      end
+    end
+    circle_area = points_in_circle.to_f / num_points
+    pi = 4 * (circle_area / square_area)
+  end
+
 end
--- pi_calculator_spec.rb
+++ pi_calculator_spec.rb
@@ -1,8 +1,17 @@
 $:<< "."
 require 'pi_calculator'
 
-describe PiCalculator, "#some_method" do
+describe PiCalculator, "#calculate" do
   it "returns something close to pi" do
-    pending "What do we call our method and how do we know it's correct?"
+    pi_calculator = PiCalculator.new
+    pi = pi_calculator.calculate
+    pi.should be_within(0.1).of(3.14)
   end
+
+  it "returns something damn close to pi" do
+    pi_calculator = PiCalculator.new
+    pi = pi_calculator.calculate
+    pi.should be_within(0.0000001).of(3.14)
+  end
+
 end
